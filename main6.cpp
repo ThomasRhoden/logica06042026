db66198d2a0354a52501f78f292f334ffb7db13b
ThomasRhoden <rhodenthomas@gmail.com>
2026-04-06 21:44:33 -0300

    commit 6

---
+++ main6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main(){
+
+    std::string nome = "Thomas";
+
+    std::cout<<nome[0];
+    //busca a letra dentro do string (0 no caso = T)
+
+    return 0;
+}
