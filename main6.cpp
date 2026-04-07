f45667389a988d7686151af04bbfa9d664c0d842
ThomasRhoden <rhodenthomas@gmail.com>
2026-04-06 21:58:46 -0300

    commit 7

--- main6.cpp
+++ main6.cpp
@@ -9,7 +9,12 @@ int main(){
     //busca a letra dentro do string (0 no caso = T)
     std::cout<<nome[1];
     //busca a letra dentro do string (1 no caso = h)
-
+    std::cout<<nome[nome.length() - 1]<<std::endl;
+    nome[0] = 'A';
+    std::cout<<nome<<std::endl;
+    std::cout<<nome.at(0)<<std::endl;
+    nome.at(0) = 'z';
+    std::cout<<nome;
 
 
     return 0;
